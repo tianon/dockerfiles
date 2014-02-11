e6a5cd2e84c8b28efb8b4ea9346d3d94f6af2c3a
Tianon Gravi <admwiggin@gmail.com>
2014-02-11 16:56:15 -0700

    add a nice script for updating the upstart Dockerfiles en-masse, and rename /etc/init/lxc.conf to /etc/init/fake-container-events.conf so that we avoid conflicts with things like installing "lxc" inside the resulting containers

---
+++ sbin-init/ubuntu/upstart/update-dockerfiles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+for v in */; do
+	v="${v%/}"
+	sed "s/%VERSION%/$v/g" Dockerfile.template > "$v/Dockerfile"
+	cp init-lxc.conf "$v/"
+done
