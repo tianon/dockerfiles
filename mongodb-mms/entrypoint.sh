43c7617042523a390e3e28962d7ef601d38baad3
Tianon Gravi <admwiggin@gmail.com>
2014-02-13 11:30:31 -0700

    of course, 10gen changed the settings.py on me...

--- mongodb-mms/entrypoint.sh
+++ mongodb-mms/entrypoint.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 set -e
 
-if [ ! "$MMS_API_KEY" ] || [ ! "$MMS_SECRET_KEY" ]; then
-	echo >&2 'one of MMS_API_KEY and MMS_SECRET_KEY was not specified'
-	echo >&2 'try something like: docker run -e MMS_API_KEY=... -e MMS_SECRET_KEY=... ...'
+: ${MMS_SERVER:=https://mms.mongodb.com}
+: ${MMS_CHECK_SSL_CERTS:=True}
+
+if [ ! "$MMS_API_KEY" ]; then
+	{
+		echo 'error: MMS_API_KEY was not specified'
+		echo 'try something like: docker run -e MMS_API_KEY=... ...'
+		echo
+		echo 'Other optional variables:'
+		echo ' - MMS_SERVER = '"$MMS_SERVER"
+		echo ' - MMS_CHECK_SSL_CERTS = '"$MMS_CHECK_SSL_CERTS"
+	} >&2
 	exit 1
 fi
 
-sed -i "s/@API_KEY@/$MMS_API_KEY/g" /mms-agent/settings.py
-sed -i "s/@SECRET_KEY@/$MMS_SECRET_KEY/g" /mms-agent/settings.py
+sed -i "s!@API_KEY@!$MMS_API_KEY!g" /mms-agent/settings.py
+sed -i "s!@MMS_SERVER@!$MMS_SERVER!g" /mms-agent/settings.py
+sed -i "s!@DEFAULT_REQUIRE_VALID_SERVER_CERTIFICATES@!$MMS_CHECK_SSL_CERTS!g" /mms-agent/settings.py
 
 exec "$@"
