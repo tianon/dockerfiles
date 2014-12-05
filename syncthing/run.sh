484a04c2ca68f51d69cdedd309a31d44e0ce692f
Tianon Gravi <admwiggin@gmail.com>
2014-12-05 11:50:12 -0700

    Add run.sh for syncthing

---
+++ syncthing/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+if [ "$HOME" = '/home/user' ]; then
+	echo >&2 'uh oh, HOME=/home/user'
+	exit 1
+fi
+
+mkdir -p "$HOME/.config/syncthing"
+
+set -x
+#	-v /etc:/host/etc \
+docker run -d \
+	--name syncthing \
+	--restart always \
+	-v "$HOME:$HOME" \
+	-v "$HOME/.config/syncthing:/home/user/.config/syncthing" \
+	--net host \
+	tianon/syncthing "$@"
+sleep 1
+docker logs syncthing
