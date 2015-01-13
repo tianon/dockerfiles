b0dedf0a3b64d80e7c40c98ef14390db0073e104
Tianon Gravi <admwiggin@gmail.com>
2015-01-12 18:48:50 -0700

    Add README and example run.sh to rtorrent

---
+++ rtorrent/README.md
@@ -0,0 +1,1 @@
+See [`Dockerfile`](https://github.com/tianon/dockerfiles/blob/master/rtorrent/Dockerfile) and [example `run.sh` script](https://github.com/tianon/dockerfiles/blob/master/rtorrent/run.sh) for usage.
---
+++ rtorrent/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+if [ "$(id -u)" != 1000 -o "$(id -g)" != 1000 ]; then
+	echo >&2 'uh, your UID and GID ought to be 1000, or permissions will likely break'
+	exit 1
+fi
+
+torrents="$HOME/downloads/torrents"
+mkdir -p "$torrents"
+
+set -x
+exec docker run -it --rm \
+	--name rtorrent \
+	-v "$torrents:/torrents" \
+	tianon/rtorrent "$@"
