21ac197b106fc91b1f8b2d1eed51268d4d1afc8e
Tianon Gravi <admwiggin@gmail.com>
2015-02-14 00:03:32 -0700

    Fixup skype/update.sh to be more defensive

--- skype/update.sh
+++ skype/update.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-url="$(curl -sS -D - -o /dev/null 'http://www.skype.com/go/getskype-linux-deb-32' | awk -F ':[[:space:]]+|\r' '$1 == "Location" { print $2; exit }')"
+url="$(curl -sS -D - -o /dev/null 'http://www.skype.com/go/getskype-linux-deb-32' | awk -F ': +|\r' '$1 == "Location" { print $2; exit }')"
 
 set -x
 sed -ri 's!^(ENV SKYPE_URL) .*!\1 '"$url"'!' Dockerfile
