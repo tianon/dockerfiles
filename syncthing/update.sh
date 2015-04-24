d151a31169c17a9aefaae44bffb580b0e3132424
Tianon Gravi <admwiggin@gmail.com>
2015-04-24 10:03:04 -0600

    Update syncthing to 0.11.0 -- YOU MUST UPDATE YOUR WHOLE CLUSTER AT ONCE

--- syncthing/update.sh
+++ syncthing/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-current="$(git ls-remote --tags https://github.com/syncthing/syncthing.git | grep -v '\^{}$' | grep -v beta | cut -d/ -f3 | sort -V | tail -1)"
+current="$(git ls-remote --tags https://github.com/syncthing/syncthing.git | grep -v '\^{}$' | grep -vE 'beta|rc' | cut -d/ -f3 | sort -V | tail -1)"
 current="${current#v}"
 
 set -x
