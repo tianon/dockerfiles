0cd9d531e417dc6b8829b4a86bd7312397e1db76
Tianon Gravi <admwiggin@gmail.com>
2015-05-23 18:01:37 -0700

    Update rtorrent run.sh to use -u

--- rtorrent/run.sh
+++ rtorrent/run.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ "$(id -u)" != 1000 -o "$(id -g)" != 1000 ]; then
-	echo >&2 'uh, your UID and GID ought to be 1000, or permissions will likely break'
-	exit 1
-fi
-
 torrents="$HOME/downloads/torrents"
 mkdir -p "$torrents/session"
 touch "$torrents/.torrent.rc"
@@ -14,4 +9,6 @@ set -x
 exec docker run -it --rm \
 	--name rtorrent \
 	-v "$torrents:/torrents" \
+	-e TERM \
+	-u "$(id -u):$(id -g)" \
 	tianon/rtorrent "$@"
