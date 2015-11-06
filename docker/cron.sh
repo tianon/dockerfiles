d0143db2f8191e7f4f87084d06fa06d23a00f5c7
Tianon Gravi <admwiggin@gmail.com>
2015-11-06 12:18:17 -0800

    Run docker/update.sh

--- docker/cron.sh
+++ docker/cron.sh
@@ -5,8 +5,7 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 set -x
 ./update.sh
-git add .
-if git commit -m 'Run docker/update.sh'; then
+if git commit -m 'Run docker/update.sh' .; then
 	git push
 fi
 
