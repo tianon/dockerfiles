297d6eb8fce7bda65a08c6d76c7d19991d8dc5cb
Tianon Gravi <admwiggin@gmail.com>
2016-05-05 19:01:14 -0700

    Better golang/tip

--- golang/tip/update.sh
+++ golang/tip/update.sh
@@ -3,8 +3,9 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
+repo="$(awk '$1 == "ENV" && $2 == "GOLANG_REPO" { print $3; exit }' Dockerfile)"
 branch="$(awk '$1 == "ENV" && $2 == "GOLANG_BRANCH" { print $3; exit }' Dockerfile)"
-commit="$(git ls-remote https://github.com/golang/go.git "refs/heads/$branch" | cut -d$'\t' -f1)"
+commit="$(git ls-remote "$repo.git" "refs/heads/$branch" | cut -d$'\t' -f1)"
 
 set -x
 sed -ri 's/^(ENV GOLANG_COMMIT) .*/\1 '"$commit"'/' Dockerfile
