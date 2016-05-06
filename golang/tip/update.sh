d00266fb3098b8082e1b563fdf43494861f775d0
Tianon Gravi <admwiggin@gmail.com>
2016-05-05 18:39:28 -0700

    Fix update.sh, update golang/tip

--- golang/tip/update.sh
+++ golang/tip/update.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-branch="$(awk '$1 == "ENV" && $2 == "GOLANG_BRANCH" { print $2; exit }' Dockerfile)"
+branch="$(awk '$1 == "ENV" && $2 == "GOLANG_BRANCH" { print $3; exit }' Dockerfile)"
 commit="$(git ls-remote https://github.com/golang/go.git "refs/heads/$branch" | cut -d$'\t' -f1)"
 
 set -x
