b1c7c7c1a961c408160d43a904428e46d00159ce
Tianon Gravi <admwiggin@gmail.com>
2016-05-05 18:39:15 -0700

    Add golang/tip/update.sh

---
+++ golang/tip/update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+branch="$(awk '$1 == "ENV" && $2 == "GOLANG_BRANCH" { print $2; exit }' Dockerfile)"
+commit="$(git ls-remote https://github.com/golang/go.git "refs/heads/$branch" | cut -d$'\t' -f1)"
+
+set -x
+sed -ri 's/^(ENV GOLANG_COMMIT) .*/\1 '"$commit"'/' Dockerfile
