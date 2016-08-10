42bb2f4185a1aba52e76cc9ab169053330b94da2
Tianon Gravi <admwiggin@gmail.com>
2016-08-10 14:10:54 -0700

    Remove "smart_push" behavior now that Hub no longer supports the v1 protocol

--- docker/push.sh
+++ docker/push.sh
@@ -11,34 +11,13 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
-token="$(curl -fsSL -o /dev/null -D- -H 'X-Docker-Token: true' "https://index.docker.io/v1/repositories/$repo/images" | tr -d '\r' | awk -F ': *' '$1 == "X-Docker-Token" { print $2 }')"
-IFS=$'\n'
-tags=( $(curl -fsSL -H "Authorization: Token $token" "https://registry-1.docker.io/v1/repositories/$repo/tags" | sed -r 's/^[{]"|"[}]$//g' | awk -v RS='", "' -F '": "' '{ print $1, $2 }') )
-unset IFS
-
-declare -A images=()
-for tagIdsF in "${tags[@]}"; do
-	tagIds=( $tagIdsF )
-	tag="${tagIds[0]}"
-	tagId="${tagIds[1]}"
-	images["$repo:$tag"]="$tagId"
-done
-
-smart_push() {
-	hubId="${images[$1]}"
-	localId="$(docker inspect -f '{{.Id}}' "$1")"
-	if [ -z "$hubId" -o "$hubId" != "$localId" ]; then
-		( set -x && docker push "$1" )
-	fi
-}
-
 for version in "${versions[@]}"; do
-	smart_push "$repo:$version"
-	smart_push "$repo:$version-dind"
-	smart_push "$repo:$version-git"
+	( set -x && docker push "$repo:$version" )
+	( set -x && docker push "$repo:$version-dind" )
+	( set -x && docker push "$repo:$version-git" )
 	if [ "$version" = 'master' ]; then
-		smart_push "$repo:latest"
-		smart_push "$repo:dind"
-		smart_push "$repo:git"
+		( set -x && docker push "$repo:latest" )
+		( set -x && docker push "$repo:dind" )
+		( set -x && docker push "$repo:git" )
 	fi
 done
