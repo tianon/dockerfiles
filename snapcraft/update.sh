617d08af2c05172a385220a749bfc156f3c90f1f
Tianon Gravi <admwiggin@gmail.com>
2016-08-23 15:49:42 -0700

    Change snapcraft's "update.sh" to check all relevant suites for newer snapcraft versions

--- snapcraft/update.sh
+++ snapcraft/update.sh
@@ -3,14 +3,34 @@ set -eo pipefail
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-current="$(
-	wget -qO- 'http://archive.ubuntu.com/ubuntu/dists/xenial/universe/binary-amd64/Packages.xz' \
-		| xz -d \
-		| awk -F ': ' '
-			$1 == "Package" { pkg = $2 }
-			$1 == "Version" && pkg == "snapcraft" { print $2 }
-		'
-)"
+mirror='http://archive.ubuntu.com/ubuntu'
+suites=( xenial xenial-updates xenial-security )
+#components=( main restricted universe )
+components=( universe )
+arch='amd64'
+
+latestVersion=
+for suite in "${suites[@]}"; do
+	for component in "${components[@]}"; do
+		url="$mirror/dists/$suite/$component/binary-$arch/Packages.xz"
+		versions=( $(
+			wget -qO- "$url" \
+				| xz -d \
+				| awk -F ': ' '
+					$1 == "Package" { pkg = $2 }
+					$1 == "Version" && pkg == "snapcraft" { print $2 }
+				'
+		) )
+		for version in "${versions[@]}"; do
+			if \
+				[ -z "$latestVersion" ] \
+				|| dpkg --compare-versions "$version" '>>' "$latestVersion" \
+			; then
+				latestVersion="$version"
+			fi
+		done
+	done
+done
 
 set -x
-sed -ri 's/^(ENV SNAPCRAFT_VERSION) .*/\1 '"$current"'/' Dockerfile
+sed -ri 's/^(ENV SNAPCRAFT_VERSION) .*/\1 '"$latestVersion"'/' Dockerfile
