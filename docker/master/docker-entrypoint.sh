822b6b1fbf7c0c30617fac67b4e168cd4e1a5a88
Tianon Gravi <admwiggin@gmail.com>
2016-12-13 15:26:50 -0800

    Backport some minor updates from the docker:1.13-rc images

--- docker/master/docker-entrypoint.sh
+++ docker/master/docker-entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 set -e
 
-if [ "${1:0:1}" = '-' ]; then
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
 	set -- docker "$@"
 fi
 
 # if our command is a valid Docker subcommand, let's invoke it through Docker instead
 # (this allows for "docker run docker ps", etc)
-if docker "$1" --help > /dev/null 2>&1; then
+if docker help "$1" > /dev/null 2>&1; then
 	set -- docker "$@"
 fi
 
