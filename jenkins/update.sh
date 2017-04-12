7c184d8e9d02dfad5b6249eb330d96debec22c9d
Tianon Gravi <admwiggin@gmail.com>
2017-04-12 10:22:36 -0700

    Switch to pkg.jenkins.io and stretch-slim

--- jenkins/update.sh
+++ jenkins/update.sh
@@ -3,6 +3,6 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-latest="$(curl -fsSL 'http://pkg.jenkins-ci.org/debian/binary/Packages.gz' | gunzip | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg == "jenkins" && $1 == "Version" { print $2; exit }')"
+latest="$(curl -fsSL 'https://pkg.jenkins.io/debian/binary/Packages.gz' | gunzip | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg == "jenkins" && $1 == "Version" { print $2; exit }')"
 set -x
 sed -ri 's/^(ENV JENKINS_VERSION) .*/\1 '"$latest"'/' Dockerfile
