4bffe177d0335d3e0e337bc7987635fc14b3fa27
Tianon Gravi <admwiggin@gmail.com>
2017-07-07 12:36:19 -0700

    Add "download-debuerreotype-tarball.sh" script for helping download released debuerreotype sbuild tarballs

---
+++ sbuild/download-debuerreotype-tarball.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+suite="$1"; shift
+hostArch="$(dpkg --print-architecture)"
+arch="${1:-$hostArch}"
+
+case "$arch" in
+	amd64|i386|s390x) bashbrewArch="$arch" ;;
+	arm64) bashbrewArch='arm64v8' ;;
+	armel) bashbrewArch='arm32v5' ;;
+	armhf) bashbrewArch='arm32v7' ;;
+	ppc64el) bashbrewArch='ppc64le' ;;
+	*) echo >&2 "error: unknown bashbrew arch for dpkg arch '$arch'"; exit 1 ;;
+esac
+
+wget -O "$suite-$arch.tar.xz.sha256" "https://github.com/debuerreotype/docker-debian-artifacts/raw/dist-$bashbrewArch/$suite/sbuild/rootfs.tar.xz.sha256"
+wget -O "$suite-$arch.tar.xz" "https://github.com/debuerreotype/docker-debian-artifacts/raw/dist-$bashbrewArch/$suite/sbuild/rootfs.tar.xz"
+sha256="$(sha256sum "$suite-$arch.tar.xz" | cut -d' ' -f1)"
+( set -x && [ "$sha256" = "$(< "$suite-$arch.tar.xz.sha256")" ] )
+
+tee "/etc/schroot/chroot.d/$suite-$arch-sbuild" <<-EOF
+	[$suite-$arch-sbuild]
+	description=Debian $suite/$arch autobuilder
+	groups=root,sbuild
+	root-groups=root,sbuild
+	profile=sbuild
+	type=file
+	file=$PWD/$suite-$arch.tar.xz
+	source-root-groups=root,sbuild
+EOF
