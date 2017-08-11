f9c544857d6959c52c0980176aa86c72b06f6c2f
Tianon Gravi <admwiggin@gmail.com>
2017-08-11 16:31:11 -0700

    Fix jenkins update.sh

--- jenkins/update.sh
+++ jenkins/update.sh
@@ -3,6 +3,15 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-latest="$(curl -fsSL 'https://pkg.jenkins.io/debian/binary/Packages.gz' | gunzip | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg == "jenkins" && $1 == "Version" { print $2; exit }')"
+latest="$(
+	curl -fsSL 'https://pkg.jenkins.io/debian/binary/Packages.gz' \
+		| gunzip \
+		| awk -F ': ' '
+			$1 == "Package" { pkg = $2 }
+			pkg == "jenkins" && $1 == "Version" { print $2 }
+		' \
+		| sort -rV \
+		| head -n1
+)"
 set -x
 sed -ri 's/^(ENV JENKINS_VERSION) .*/\1 '"$latest"'/' Dockerfile
