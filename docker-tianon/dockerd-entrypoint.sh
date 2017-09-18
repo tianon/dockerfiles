287456c3c38cf5ca202c56b5da8b91e5d2ed3976
Tianon Gravi <admwiggin@gmail.com>
2017-09-18 10:59:13 -0700

    Create a new "docker-tianon" image based on https://doi-janky.infosiftr.net/job/tianon/job/docker-deb/job/repo/ (which builds artifacts from https://github.com/tianon/docker-deb-vendored)

---
+++ docker-tianon/dockerd-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+# no arguments passed
+# or first arg is `-f` or `--some-option`
+if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
+	# add our default arguments
+	set -- dockerd \
+		--host=unix:///var/run/docker.sock \
+		--host=tcp://0.0.0.0:2375 \
+		"$@"
+fi
+
+# if our command is a valid Docker subcommand, let's invoke it through Docker instead
+# (this allows for "docker run docker ps", etc)
+if docker help "$1" > /dev/null 2>&1; then
+	set -- docker "$@"
+fi
+
+# if we have "--link some-docker:docker" and not DOCKER_HOST, let's set DOCKER_HOST automatically
+if [ -z "${DOCKER_HOST:-}" ] && [ -n "${DOCKER_PORT_2375_TCP:-}" ]; then
+	export DOCKER_HOST='tcp://docker:2375'
+fi
+
+exec "$@"
