f4999a30055dfbf9acb1ae8d38000ee26e3384dc
Tianon Gravi <admwiggin@gmail.com>
2018-01-05 16:48:52 -0800

    Adjust Plex update.sh method (no more XML!)
    Thanks to https://github.com/mrworf/plexupdate/blob/ec5e3b7fa45693bf5a5d383831a39068ec4e7b2e/plexupdate-core#L14 for the JSON URL.

--- plex/media-server/update.sh
+++ plex/media-server/update.sh
@@ -3,7 +3,19 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-version="$(curl -fsSL 'https://plex.tv/downloads/details/1?build=linux-ubuntu-x86_64&channel=16&distro=ubuntu' | sed -n 's/.*Release.*version="\([^"]*\)".*/\1/p')"
+#version="$(curl -fsSL 'https://plex.tv/downloads/details/1?build=linux-ubuntu-x86_64&channel=16&distro=ubuntu' | sed -n 's/.*Release.*version="\([^"]*\)".*/\1/p')"
+
+json="$(curl -fsSL 'https://plex.tv/api/downloads/1.json' | jq '.computer.Linux')"
+
+version="$(echo "$json" | jq -r '.version')"
+
+ubuntuRelease="$(echo "$json" | jq '.releases[] | select(.distro == "ubuntu" and .build == "linux-ubuntu-x86_64")')"
+url="$(echo "$ubuntuRelease" | jq -r '.url')"
+sha1="$(echo "$ubuntuRelease" | jq -r '.checksum')"
 
 set -x
-sed -ri 's!^(ENV PLEX_VERSION) .*!\1 '"$version"'!' Dockerfile
+sed -ri \
+	-e 's!^(ENV PLEX_VERSION) .*!\1 '"$version"'!' \
+	-e 's!^(ENV PLEX_URL) .*!\1 '"$url"'!' \
+	-e 's!^(ENV PLEX_SHA1) .*!\1 '"$sha1"'!' \
+	Dockerfile
