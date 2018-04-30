16243404892133d40b6276ccdd902762e1e115be
Tianon Gravi <admwiggin@gmail.com>
2018-04-30 09:39:19 -0700

    Update Google Music Manager to 1.0.467.4929-r0 (and stretch-slim)

--- google-musicmanager/update.sh
+++ google-musicmanager/update.sh
@@ -3,6 +3,12 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-latest="$(curl -fsSL 'http://dl.google.com/linux/musicmanager/deb/dists/stable/main/binary-amd64/Packages.bz2' | bunzip2 | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg == "google-musicmanager-beta" && $1 == "Version" { print $2; exit }')"
+latest="$(
+	curl -fsSL 'http://dl.google.com/linux/musicmanager/deb/dists/stable/main/binary-amd64/Packages' \
+		| awk -F ': ' '
+			$1 == "Package" { pkg = $2 }
+			pkg == "google-musicmanager-beta" && $1 == "Version" { print $2; exit }
+		'
+)"
 set -x
-sed -ri 's/^(ENV GMM_VERSION) .*/\1 '"$latest"'/' Dockerfile
+sed -ri 's/^(ENV GOOGLE_MUSICMANAGER_VERSION) .*/\1 '"$latest"'/' Dockerfile
