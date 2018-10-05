7c4a56ed1991d89abf6e775a8823ae5894ca6892
Tianon Gravi <admwiggin@gmail.com>
2018-10-05 11:17:39 -0700

    Add "backblaze-b2" image

---
+++ backblaze-b2/update.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+cd "$(dirname "$BASH_SOURCE")"
+
+current="$(curl -fsSL 'https://pypi.org/pypi/b2/json' | jq -r .info.version)"
+
+set -x
+sed -ri 's/^(ENV B2_VERSION) .*/\1 '"$current"'/' Dockerfile
