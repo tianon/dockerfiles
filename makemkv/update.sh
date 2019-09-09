cae04cf800de7599d8aacc64bf7a775c26440e1a
Tianon Gravi <admwiggin@gmail.com>
2019-09-09 08:12:44 -0700

    Update MakeMKV to 1.14.5

--- makemkv/update.sh
+++ makemkv/update.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 url="$(
 	curl -fsSL 'https://www.makemkv.com/forum/viewtopic.php?f=3&t=224' \
-		| grep -oE 'href="http://[^"]+/makemkv-bin-[^"]+.tar.gz"' \
+		| grep -oE 'href="https://[^"]+/makemkv-bin-[^"]+.tar.gz"' \
 		| cut -d'"' -f2
 )"
 version="$(basename "$url" | sed -r 's!^makemkv-bin-|.tar.gz$!!g')"
