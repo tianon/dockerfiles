9ee21ca71a2fee9c1e7a3d8f8f2f53def4011f73
J0WI <J0WI@users.noreply.github.com>
2019-09-19 15:09:29 +0200

    Fix speedtest-cli update.sh

--- speedtest/update.sh
+++ speedtest/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-current="$(curl -sSL 'https://pypi.python.org/pypi/speedtest-cli/json' | awk -F '"' '$2 == "version" { print $4 }')"
+current="$(curl -sSL 'https://pypi.python.org/pypi/speedtest-cli/json' | jq .info.version | tr -d '"')"
 
 set -x
-sed -ri 's/^(ENV SPEEDTEST_CLI_VERSION) .*/\1 '"$current"'/' Dockerfile
+sed -ri 's/^(ENV SPEEDTEST_VERSION) .*/\1 '"$current"'/' Dockerfile
