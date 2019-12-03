5c1c1e13f0172670dd20aeceab7d32a33444f044
Tianon Gravi <admwiggin@gmail.com>
2019-12-03 15:48:19 -0800

    Swap wine to use upstream's packages

--- wine/32/Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM debian:stretch-slim
-
-RUN dpkg --add-architecture i386 \
-	&& apt-get update \
-	&& apt-get install -y --no-install-recommends \
-		wine32 \
-		wine \
-	&& rm -rf /var/lib/apt/lists/*
--- wine/gsl.sh
+++ wine/gsl.sh
@@ -1,12 +1,1 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-
-cd "$(dirname "$BASH_SOURCE")"
-dir="$(basename "$PWD")"
-cd ..
-
-source gsl-libs.sh
-
-globalEntry
-tagsEntry "$dir/32" 32 latest
-tagsEntry "$dir/64" 64
+../.gsl-common/gsl-version-env.sh
