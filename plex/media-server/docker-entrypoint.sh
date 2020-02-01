24c5c9e585ff82e43227c4b79c8b04d144f3dc70
Tianon Gravi <admwiggin@gmail.com>
2020-01-31 17:26:53 -0800

    Update Plex Media Server to Debian Buster

--- plex/media-server/docker-entrypoint.sh
+++ plex/media-server/docker-entrypoint.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -Eeuo pipefail
 
 # delete stale pid file so Plex doesn't fail to start
 rm -f "$PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR/Plex Media Server/plexmediaserver.pid"
