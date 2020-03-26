7582b49cee17149e7744b1a96adab3dd9cddf166
Tianon Gravi <admwiggin@gmail.com>
2020-03-26 16:48:38 -0700

    Switch gitlab mirroring from Docker to containerd

--- gitlab/retag.sh
+++ gitlab/retag.sh
@@ -4,6 +4,23 @@ set -Eeuo pipefail
 src='gitlab/gitlab-ce'
 dst='tianon/gitlab'
 
+# before running this script, I start (unprivileged) containerd in a separate container:
+#   docker run -dit --name containerd --restart always --user nobody --security-opt no-new-privileges tianon/containerd
+# https://hub.docker.com/r/tianon/containerd
+# (containerd makes mirroring existing images verbatim orders of magnitude simpler :D)
+
+ctr() {
+	tty=
+	if [ -t 0 ] && [ -t 1 ]; then
+		tty='--tty'
+	fi
+	docker exec $tty --interactive containerd ctr "$@"
+}
+
+# get Docker Hub credentials from ~/.docker/config.json O:)
+auth="$(jq -r '.auths."https://index.docker.io/v1/".auth' ~/.docker/config.json | base64 -d)"
+[ -n "$auth" ]
+
 tags="$(
 	# https://github.com/tianon/docker-bin/blob/master/docker-hub-list-tags.sh
 	docker-hub-list-tags.sh "$src" \
@@ -36,9 +53,16 @@ done
 
 for dstTag in "${!latest[@]}"; do
 	srcTag="${latest[$dstTag]}"
-	( set -x; docker pull "$src:$srcTag" )
-	( set -x; docker tag "$src:$srcTag" "$dst:$dstTag" )
+	ctrSrc="docker.io/$src:$srcTag"
+	ctrDst="docker.io/$dst:$dstTag"
+	echo
+	echo "+ docker pull $src:$srcTag"
+	ctr content fetch --metadata-only "$ctrSrc"
+	echo
+	echo "+ docker tag $src:$srcTag $dst:$dstTag"
 	if [ "${1:-}" = '--push' ]; then
-		( set -x; docker push "$dst:$dstTag" )
+		echo
+		echo "+ docker push $dst:$dstTag"
+		ctr images push --user "$auth" "$ctrDst" "$ctrSrc"
 	fi
 done
