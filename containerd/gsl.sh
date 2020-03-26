f84280ef47393b585e38e86994de77d605f11028
Tianon Gravi <admwiggin@gmail.com>
2020-03-26 16:17:35 -0700

    Add containerd

---
+++ containerd/gsl.sh
@@ -0,0 +1,1 @@
+../.gsl-common/gsl-version-env.sh
---
+++ containerd/non-root-containerd.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+uid="$(id -u)"
+gid="$(id -g)"
+if [ "$uid" = 0 ]; then
+	# if we're root, nothing special is required
+	exec containerd "$@"
+fi
+
+# if we're not root, let's adjust all the "uid" and "gid" parameters of the config to whatever our current user is (so we avoid "chown" permission errors)
+exec containerd --config <(containerd config dump | awk -v uid="$(id -u)" -v gid="$(id -g)" '$1 == "uid" { gsub(/=.+$/, "= " uid) } $1 == "gid" { gsub(/=.+$/, "= " gid) } { print }') "$@"
