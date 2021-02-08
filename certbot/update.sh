1abcab51ea9b28057f9771c82a6404ce239ecbeb
Tianon Gravi <admwiggin@gmail.com>
2021-02-08 09:46:33 -0800

    Remove certbot
    I don't personally use this anymore, preferring instead the simplicity of https://github.com/diafygi/acme-tiny and friends.

--- certbot/update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-
-cd "$(dirname "$BASH_SOURCE")"
-
-current="$(curl -fsSL 'https://pypi.org/pypi/certbot/json' | jq -r .info.version)"
-
-set -x
-sed -ri 's/^(ENV CERTBOT_VERSION) .*/\1 '"$current"'/' Dockerfile
