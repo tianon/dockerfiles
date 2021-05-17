0936395cded663114eb7d0b2bc039876e8e4ffc7
Tianon Gravi <admwiggin@gmail.com>
2021-05-17 10:05:52 -0700

    Remove (deprecated) google-cloud-print-connector 😞

--- google-cloud-print-connector/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-
-# "A connector is already running, or the monitoring socket /tmp/cloud-print-connector-monitor.sock wasn't cleaned up properly"
-rm -f /tmp/cloud-print-connector-monitor.sock
-
-exec "$@"
