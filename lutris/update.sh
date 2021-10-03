a83483ed40f839ac514ac3f5c44e5686c22e8170
Tianon Gravi <admwiggin@gmail.com>
2021-10-02 22:33:21 -0700

    Add winetricks to lutris update.sh

--- lutris/update.sh
+++ lutris/update.sh
@@ -12,8 +12,19 @@ version="$(
 		| head -1
 )"
 
-echo "lutris: $version"
+winetricks="$(
+	git ls-remote --tags https://github.com/Winetricks/winetricks.git \
+		| sed \
+			-e 's!^.*/!!' \
+			-e 's/\^.*$//' \
+			-e 's/^v//' \
+		| sort -ruV \
+		| head -1
+)"
+
+echo "lutris: $version (winetricks $winetricks)"
 
 sed -ri \
 	-e 's!^(ENV LUTRIS_VERSION) .*!\1 '"$version"'!' \
+	-e 's!^(ENV WINETRICKS_VERSION) .*!\1 '"$winetricks"'!' \
 	Dockerfile
