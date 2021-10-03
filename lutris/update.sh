c763f66f5c855f8c42c77b27d1082f1b588b1ba5
Tianon Gravi <admwiggin@gmail.com>
2021-10-02 22:31:43 -0700

    Fix lutris/update.sh

--- lutris/update.sh
+++ lutris/update.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-suite="$(gawk -F '[[:space:]:]+' '$1 == "FROM" { print $3; exit }' Dockerfile)"
-
-ppaVersion="$(
-	# https://launchpad.net/~lutris-team/+archive/ubuntu/lutris
-	wget -qO- "http://ppa.launchpad.net/lutris-team/lutris/ubuntu/dists/$suite/main/binary-amd64/Packages.xz" \
-		| xz -d \
-		| tac|tac \
-		| gawk -F ':[[:space:]]+' '
-			$1 == "Package" { pkg = $2 }
-			$1 == "Version" && pkg == "lutris" { print $2; exit }
-		'
+version="$(
+	git ls-remote --tags https://github.com/lutris/lutris.git \
+		| sed \
+			-e 's!^.*/!!' \
+			-e 's/\^.*$//' \
+			-e 's/^v//' \
+		| sort -ruV \
+		| grep -vE 'beta' \
+		| head -1
 )"
-tilde='~'
-version="${ppaVersion%$tilde*}"
 
-echo "lutris: $version ($ppaVersion)"
+echo "lutris: $version"
 
 sed -ri \
 	-e 's!^(ENV LUTRIS_VERSION) .*!\1 '"$version"'!' \
-	-e 's!^(ENV LUTRIS_PPA_VERSION) .*!\1 '"$ppaVersion"'!' \
 	Dockerfile
