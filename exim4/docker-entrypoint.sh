5eadc280e1f8824769b22db31ddbb15477603c5d
Tianon Gravi <admwiggin@gmail.com>
2021-10-23 13:26:15 -0700

    Give exim4 better defaults (so there's less to do at runtime)

--- exim4/docker-entrypoint.sh
+++ exim4/docker-entrypoint.sh
@@ -2,33 +2,22 @@
 set -e
 
 if [ "$1" = 'exim' ]; then
-	opts=(
-		dc_local_interfaces '0.0.0.0 ; ::0'
-		dc_other_hostnames ''
-		dc_relay_nets '0.0.0.0/0'
-	)
-
-	if [ "$GMAIL_USER" -a "$GMAIL_PASSWORD" ]; then
+	if [ -n "$GMAIL_USER" ] && [ -n "$GMAIL_PASSWORD" ]; then
 		# see https://wiki.debian.org/GmailAndExim4
-		opts+=(
-			dc_eximconfig_configtype 'smarthost'
+		set-exim4-update-conf \
+			dc_eximconfig_configtype 'smarthost' \
 			dc_smarthost 'smtp.gmail.com::587'
-		)
 		echo "smtp.gmail.com:$GMAIL_USER:$GMAIL_PASSWORD" > /etc/exim4/passwd.client
-	else
-		opts+=(
-			dc_eximconfig_configtype 'internet'
-		)
 	fi
-
-	set-exim4-update-conf "${opts[@]}"
 
 	if [ "$(id -u)" = '0' ]; then
 		mkdir -p /var/spool/exim4 /var/log/exim4 || :
 		chown -R Debian-exim:Debian-exim /var/spool/exim4 /var/log/exim4 || :
 	fi
 
-	set -- tini -- "$@"
+	if [ "$$" = 1 ]; then
+		set -- tini -- "$@"
+	fi
 fi
 
 exec "$@"
