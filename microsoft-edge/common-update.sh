dccdc3b7743672ff5b2a4beed2fcc00878bdc51a
Tianon Gravi <admwiggin@gmail.com>
2021-11-01 12:44:39 -0700

    Make microsoft-edge "update.sh" more reliable

--- microsoft-edge/common-update.sh
+++ microsoft-edge/common-update.sh
@@ -3,15 +3,15 @@ set -Eeuo pipefail
 
 channel="$(basename "$PWD")" # "dev", etc
 
-version="$(
+versions="$(
 	wget -qO- 'https://packages.microsoft.com/repos/edge/dists/stable/main/binary-amd64/Packages.gz' \
 		| gunzip \
-		| tac|tac \
 		| gawk -F ':[[:space:]]+' -v channel="$channel" '
 			$1 == "Package" { pkg = $2 }
-			$1 == "Version" && pkg == "microsoft-edge-" channel { print $2; exit }
+			$1 == "Version" && pkg == "microsoft-edge-" channel { print $2 }
 		'
 )"
+version="$(head -1 <<<"$versions")"
 
 echo "microsoft-edge-$channel: $version"
 [ -n "$version" ]
