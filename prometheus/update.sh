f3e3f1dc5da1ade92c0721a967055d2e9c0d0dae
J0WI <J0WI@users.noreply.github.com>
2021-12-17 19:49:48 +0100

    Update Prometheus

--- prometheus/update.sh
+++ prometheus/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-latest="$(git ls-remote --tags https://github.com/prometheus/prometheus.git | cut -d/ -f3 | cut -d^ -f1 | sort -V | grep -v rc | tail -1)"
+latest="$(git ls-remote --tags https://github.com/prometheus/prometheus.git | cut -d/ -f3 | cut -d^ -f1 | grep -v -E "(beta|rc)" | sort -V | tail -1)"
 latest="${latest#v}"
 
 dir="$(dirname "$BASH_SOURCE")"
