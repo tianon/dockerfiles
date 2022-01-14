329ca7ba603f022f599586576237d8aa74320c8c
Tianon Gravi <admwiggin@gmail.com>
2022-01-13 20:55:46 -0800

    Update HandBrake to 1.5.1

--- handbrake/update.sh
+++ handbrake/update.sh
@@ -1,42 +1,16 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-suite="$(gawk -F '[[:space:]:]+' '$1 == "FROM" { print $3; exit }' Dockerfile)"
-
 version="$(
-	# https://launchpad.net/~stebbins/+archive/ubuntu/handbrake-releases
-	wget -qO- "http://ppa.launchpad.net/stebbins/handbrake-releases/ubuntu/dists/$suite/main/binary-amd64/Packages.xz" \
-		| xz -d \
-		| tac|tac \
-		| gawk -F ':[[:space:]]+' '
-			$1 == "Package" { pkg = $2 }
-			$1 == "Version" {
-				if (pkg == "handbrake-cli") {
-					cli = $2
-					next
-				}
-				if (pkg == "handbrake-gtk") {
-					gtk = $2
-					next
-				}
-			}
-			END {
-				if (cli && cli == gtk) {
-					print cli
-					exit 0
-				} else {
-					printf "error: version missing/mismatch!\n\t\"%s\" vs \"%s\" (cli vs gtk)\n", cli, gtk > "/dev/stderr"
-					exit 1
-				}
-			}
-		'
+	git ls-remote --tags https://github.com/HandBrake/HandBrake.git \
+		| cut -d/ -f3 \
+		| cut -d^ -f1 \
+		| sort -V \
+		| tail -1
 )"
-cleanVersion="${version##*:}"
-cleanVersion="${cleanVersion%%-*}"
 
-echo "handbrake: $cleanVersion ($version)"
+echo "handbrake: $version"
 
 sed -ri \
-	-e 's!^(ENV HANDBRAKE_VERSION) .*!\1 '"$cleanVersion"'!' \
-	-e 's!^(ENV HANDBRAKE_PPA_VERSION) .*!\1 '"$version"'!' \
+	-e 's!^(ENV HANDBRAKE_VERSION) .*!\1 '"$version"'!' \
 	Dockerfile
