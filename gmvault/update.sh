bb5f8da87ad856c657723eda2c306ce4872964f3
Tianon Gravi <admwiggin@gmail.com>
2022-03-11 14:57:49 -0800

    Remove gmvault 😞

--- gmvault/update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-
-cd "$(dirname "$BASH_SOURCE")"
-
-current="$(curl -fsSL 'https://pypi.org/pypi/gmvault/json' | jq -r .info.version)"
-
-set -x
-sed -ri 's/^(ENV GMVAULT_VERSION) .*/\1 '"$current"'/' Dockerfile
