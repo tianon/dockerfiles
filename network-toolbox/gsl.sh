074c8972b858eac58fe950b43e18381318b24b8f
Tianon Gravi <admwiggin@gmail.com>
2022-03-11 16:54:04 -0800

    Add "suite aliases" to network-toolbox

--- network-toolbox/gsl.sh
+++ network-toolbox/gsl.sh
@@ -2,11 +2,18 @@
 set -Eeuo pipefail
 
 cd "$(dirname "$BASH_SOURCE")"
+
 dir="$(basename "$PWD")"
+
+suite="$(gawk -F '[[:space:]:]+' '$1 == "FROM" { print $3; exit }' debian/Dockerfile)"
+suite="${suite%-slim}"
+
+alpine="$(gawk -F '[[:space:]:]+' '$1 == "FROM" { print $3; exit }' alpine/Dockerfile)"
+
 cd ..
 
 source gsl-libs.sh
 
 globalEntry
-tagsEntry "$dir/alpine" alpine
-tagsEntry "$dir/debian" debian latest
+tagsEntry "$dir/alpine" alpine "alpine$alpine"
+tagsEntry "$dir/debian" debian "$suite" latest
