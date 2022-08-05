6d6e6561de162f7af20e491a2ced064d0db5cea1
Tianon Gravi <admwiggin@gmail.com>
2022-08-05 16:17:45 -0700

    Add TODO

--- tailscale/versions.sh
+++ tailscale/versions.sh
@@ -9,4 +9,6 @@ source "$dir/../.libs/git.sh"
 
 json="$(git-tags 'https://github.com/tailscale/tailscale.git')"
 
+# TODO verify that the scraped version exists on https://pkgs.tailscale.com/stable/#static
+
 jq <<<"$json" -S 'del(.tag)' > versions.json
