717fecbbdd69a4a235963502e6d532bba42940a4
Tianon Gravi <admwiggin@gmail.com>
2022-10-04 13:40:45 -0700

    Update tinygo to scrape Go version from go.mod (just for a stable/accurate answer)

--- tinygo/versions.sh
+++ tinygo/versions.sh
@@ -8,5 +8,11 @@ dir="$(dirname "$dir")"
 source "$dir/../.libs/git.sh"
 
 json="$(git-tags 'https://github.com/tinygo-org/tinygo.git')"
+tag="$(jq <<<"$json" -r '.tag')"
+go="$(
+	wget -qO- "https://github.com/tinygo-org/tinygo/raw/$tag/go.mod" \
+		| awk '$1 == "go" { print $2; exit }'
+)"
+echo "tinygo go: $go"
 
-jq <<<"$json" -S 'del(.tag)' > versions.json
+jq <<<"$json" -S --arg go "$go" 'del(.tag) | .go = { version: $go }' > versions.json
