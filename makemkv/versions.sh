0f4afef19031d81d5267ec5f15ecfdf7cf9d18a9
Tianon Gravi <admwiggin@gmail.com>
2022-10-12 14:07:33 -0700

    Scrape MakeMKV versions from https://makemkv.com/download/ first and https://forum.makemkv.com/forum/viewtopic.php?f=3&t=224 only as a fallback

--- makemkv/versions.sh
+++ makemkv/versions.sh
@@ -3,12 +3,23 @@ set -Eeuo pipefail
 
 [ -e versions.json ]
 
-url="$(
-	curl -fsSL 'https://forum.makemkv.com/forum/viewtopic.php?f=3&t=224' \
-		| grep -oE 'href="https?://[^"]+/makemkv-bin-[^"]+.tar.gz"' \
-		| cut -d'"' -f2
-)"
-version="$(basename "$url" | sed -r 's!^makemkv-bin-|.tar.gz$!!g')"
+{
+	version="$(
+		curl -fsSL 'https://makemkv.com/download/' \
+			| grep -oE 'makemkv-sha-[0-9.]+.txt' \
+			| sed -r 's!^makemkv-sha-|[.]txt$!!g'
+	)"
+	[ -n "$version" ]
+} || {
+	url="$(
+		curl -fsSL 'https://forum.makemkv.com/forum/viewtopic.php?f=3&t=224' \
+			| grep -oE 'href="https?://[^"]+/makemkv-bin-[^"]+.tar.gz"' \
+			| cut -d'"' -f2
+	)"
+	[ -n "$url" ]
+	version="$(basename "$url" | sed -r 's!^makemkv-bin-|[.]tar[.]gz$!!g')"
+	[ -n "$version" ]
+}
 export version
 
 echo >&2 "makemkv: $version"
