81a3d6fcb5ab4ea63cf2190037284173511995d9
Tianon Gravi <admwiggin@gmail.com>
2023-08-10 10:20:40 -0700

    Add cygwin TODO

--- cygwin/versions.sh
+++ cygwin/versions.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+# TODO https://cygwin.com/setup/?C=M;O=D !!!!
+
 [ -e versions.json ]
 
 sha512="$(wget -qO- 'https://cygwin.com/sha512.sum' | awk -v ret=1 '$2 == "setup-x86_64.exe" { print $1; ret = 0; exit ret } END { exit ret }')"
