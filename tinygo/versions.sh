567f52474a6e029b27a58f9ac0b5dfae2dbd33da
Tianon Gravi <admwiggin@gmail.com>
2023-08-31 08:14:56 -0700

    Add tinygo TODO

--- tinygo/versions.sh
+++ tinygo/versions.sh
@@ -17,6 +17,7 @@ go="$(
 		| awk '$1 == "go" { print $2; exit }'
 )"
 echo "tinygo go: $go"
+# TODO this gives us the "minimal" version of Go, where it would be great if we could somehow parse https://github.com/tinygo-org/tinygo/blob/dc449882ad09c60c11cef7c35914d5fbfe22a88e/builder/config.go#L33 and get the "maximal" version instead
 
 jq <<<"$json" --arg go "$go" '
 	.go = { version: $go }
