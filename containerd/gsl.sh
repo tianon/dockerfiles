6b14381df1a2160153c38a6b282bbc21aad1b068
Tianon Gravi <admwiggin@gmail.com>
2023-08-31 12:01:46 -0700

    Add c8dind variant of tianon/containerd

--- containerd/gsl.sh
+++ containerd/gsl.sh
@@ -1,1 +1,15 @@
-../.gsl-common/gsl-version-env.sh
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+cd "$(dirname "$BASH_SOURCE")"
+dir="$(basename "$PWD")"
+cd ..
+
+version="$(jq -r '.version' "$dir/versions.json")"
+
+source gsl-libs.sh
+
+globalEntry
+
+versionedTagsEntry "$dir" "$version" latest
+dockerfile='Dockerfile.c8dind' versionedVariantEntry "$dir" c8dind "$version" c8dind
