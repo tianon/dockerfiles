dc81b3fd86f08f335d5f40e7e18e0a80cf77556e
Tianon Gravi <admwiggin@gmail.com>
2023-09-13 23:29:13 -0700

    Add old per-arch tags to infosiftr-moby (for backwards compat)

--- infosiftr-moby/gsl.sh
+++ infosiftr-moby/gsl.sh
@@ -9,13 +9,25 @@ version="$(jq -r '.version' "$dir/versions.json")"
 [ -n "$version" ]
 
 from="$(awk '$1 == "FROM" { print $2; exit }' "$dir/Dockerfile")" # TODO multi-stage build??
-arches="$(bashbrew remote arches --json "$from" | jq -r '.arches | keys + ["riscv64"] | join(", ")')"
-[ -n "$arches" ]
+arches="$(bashbrew remote arches --json "$from" | jq -c '.arches | keys + ["riscv64"]')"
+archesString="$(jq <<<"$arches" -r 'join(", ")')"
+[ -n "$archesString" ]
 
 source gsl-libs.sh
 
 globalEntry
-echo "Architectures: $arches"
+echo "Architectures: $archesString"
 
 versionedTagsEntry "$dir" "$version" latest
 echo 'riscv64-File: Dockerfile.unstable'
+
+# add old per-arch tags: https://explore.ggcr.dev/?repo=infosiftr/moby
+archArr="$(jq <<<"$arches" -r 'map(@sh) | join(" ")')"
+eval "archArr=( $archArr )"
+for arch in "${archArr[@]}"; do
+	tagsEntry "$dir" "$arch"
+	echo "Architectures: $arch"
+	if [ "$arch" = 'riscv64' ]; then
+		echo 'riscv64-File: Dockerfile.unstable'
+	fi
+done
