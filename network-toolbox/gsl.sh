f5fd45871243443ca2c578332f5caf87315029a6
Tianon Gravi <admwiggin@gmail.com>
2023-09-16 00:30:02 -0700

    Multiarch network-toolbox

--- network-toolbox/gsl.sh
+++ network-toolbox/gsl.sh
@@ -15,5 +15,17 @@ cd ..
 source gsl-libs.sh
 
 globalEntry
-tagsEntry "$dir/alpine" alpine "alpine$alpine"
+
 tagsEntry "$dir/debian" debian "$suite" latest
+from="$(awk '$1 == "FROM" { print $2; exit }' "$dir/debian/Dockerfile")" # TODO multi-stage build??
+arches="$(bashbrew remote arches --json "$from" | jq -c '.arches | keys')"
+archesString="$(jq <<<"$arches" -r 'join(", ")')"
+[ -n "$archesString" ]
+echo "Architectures: $archesString"
+
+tagsEntry "$dir/alpine" alpine "alpine$alpine"
+from="$(awk '$1 == "FROM" { print $2; exit }' "$dir/alpine/Dockerfile")" # TODO multi-stage build??
+arches="$(bashbrew remote arches --json "$from" | jq -c '.arches | keys')"
+archesString="$(jq <<<"$arches" -r 'join(", ")')"
+[ -n "$archesString" ]
+echo "Architectures: $archesString"
