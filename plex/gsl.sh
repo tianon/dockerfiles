587cbe86fdd007a25805842c8a8c84fc69a8b1b7
Tianon Gravi <admwiggin@gmail.com>
2023-09-15 23:52:29 -0700

    Add Architectures: for plex:media-server

--- plex/gsl.sh
+++ plex/gsl.sh
@@ -10,3 +10,18 @@ source gsl-libs.sh
 globalEntry
 
 versionedEnvVariantEntry "$dir/media-server" media-server 'PLEX_VERSION' media-server
+from="$(awk '$1 == "FROM" { print $2; exit }' "$dir/media-server/Dockerfile")" # TODO multi-stage build??
+fromArches="$(bashbrew remote arches --json "$from" | jq -c '.arches | keys')"
+arches="$(jq -r -L "$dir/../.libs" --argjson fromArches "$fromArches" '
+	include "lib"
+	;
+	[
+		$fromArches,
+		(.arches | keys),
+		empty
+	]
+	| intersection
+	| join(", ")
+' "$dir/media-server/versions.json")"
+[ -n "$arches" ]
+echo "Architectures: $arches"
