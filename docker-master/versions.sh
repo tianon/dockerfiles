f9059970552fc7a5500162886dd2291a7989c6c1
Tianon Gravi <admwiggin@gmail.com>
2023-09-28 11:59:08 -0700

    Update docker-master (with scraped Go versions)

--- docker-master/versions.sh
+++ docker-master/versions.sh
@@ -11,6 +11,30 @@ moby="$(git-ref-commit 'https://github.com/moby/moby.git' 'HEAD')"
 cli="$(git-ref-commit 'https://github.com/docker/cli.git' 'HEAD')"
 buildx="$(git-ref-commit 'https://github.com/docker/buildx.git' 'HEAD')"
 
+moby="$(
+	commit="$(jq <<<"$moby" -r '.version')"
+	go="$(wget -qO- "https://github.com/moby/moby/raw/$commit/vendor.mod")"
+	go="$(awk <<<"$go" '$1 == "go" { print $2; exit }')"
+	echo >&2 "moby go: $go"
+	jq <<<"$moby" -c --arg go "$go" '.go = { version: $go }'
+)"
+
+cli="$(
+	commit="$(jq <<<"$cli" -r '.version')"
+	go="$(wget -qO- "https://github.com/docker/cli/raw/$commit/vendor.mod")"
+	go="$(awk <<<"$go" '$1 == "go" { print $2; exit }')"
+	echo >&2 "cli go: $go"
+	jq <<<"$cli" -c --arg go "$go" '.go = { version: $go }'
+)"
+
+buildx="$(
+	commit="$(jq <<<"$buildx" -r '.version')"
+	go="$(wget -qO- "https://github.com/docker/buildx/raw/$commit/go.mod")"
+	go="$(awk <<<"$go" '$1 == "go" { print $2; exit }')"
+	echo >&2 "buildx go: $go"
+	jq <<<"$buildx" -c --arg go "$go" '.go = { version: $go }'
+)"
+
 jq <<<"$moby" --argjson cli "$cli" --argjson buildx "$buildx" '
 	. += {
 		cli: $cli,
