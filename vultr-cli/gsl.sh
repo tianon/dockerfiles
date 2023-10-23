3bf5bae513af8d53f6c95e766bf33e1d288f0851
Tianon Gravi <admwiggin@gmail.com>
2023-10-22 21:01:05 -0700

    Update vultr-cli to be properly multiarch and slightly more resilient

--- vultr-cli/gsl.sh
+++ vultr-cli/gsl.sh
@@ -1,1 +1,29 @@
-../.gsl-common/gsl-version-env.sh
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+cd "$(dirname "$BASH_SOURCE")"
+dir="$(basename "$PWD")"
+cd ..
+
+from="$(awk '$1 == "FROM" { print $2; exit }' "$dir/Dockerfile")" # TODO multi-stage build??
+fromArches="$(bashbrew remote arches --json "$from" | jq -c '.arches | keys')"
+arches="$(jq -r -L "$dir/../.libs" --argjson fromArches "$fromArches" '
+	include "lib"
+	;
+	[
+		$fromArches,
+		(.arches | map_values(select(.apkArch)) | keys),
+		empty
+	]
+	| intersection
+	| join(", ")
+' "$dir/versions.json")"
+[ -n "$arches" ]
+
+source gsl-libs.sh
+
+globalEntry
+env="${dir^^}_VERSION"
+env="${env//-/_}"
+versionedEnvTagsEntry "$dir" "$env" latest
+echo "Architectures: $arches"
--- vultr-cli/versions.sh
+++ vultr-cli/versions.sh
@@ -14,16 +14,56 @@ json="$(
 		local tag
 		tag="$(jq <<<"$json" -r '.tag')" || return "$?"
 
-		local checksums
-		checksums="$(wget -qO- "https://github.com/vultr/vultr-cli/releases/download/$tag/vultr-cli_v${version}_checksums.txt")" || return "$?"
+		local urlBase="https://github.com/vultr/vultr-cli/releases/download/$tag"
 
-		jq <<<"$checksums" -sR '
-			rtrimstr("\n")
+		local checksums
+		checksums="$(wget -qO- "$urlBase/vultr-cli_v${version}_checksums.txt")" || return "$?"
+
+		jq <<<"$checksums" -Rsc -L../.libs --arg urlBase "$urlBase" '
+			include "lib"
+			;
+			(
+				[
+					"^(?<sha256>[0-9a-f]{64})",
+					"(  | [*])",
+					"(?<file>",
+						"(vultr-cli_)?",
+						"v?[0-9.-]+",
+						"_(?i:(?<os>linux|windows|macos))_",
+						"(?<arch>[^_. ]+)",
+						"[.](tar[.]gz|zip)",
+					")$"
+				] | join("")
+			) as $regex
+			| rtrimstr("\n")
 			| split("\n")
-			| map(capture("^(?<sha256>[0-9a-f]{64})(  | [*])(?<file>.*)$"))
-			| reduce .[] as $i ({}; .[$i.file] = $i.sha256)
-			| { sha256: . }
-			# TODO urls?
+			| map(
+				capture($regex)
+				| (
+					(
+						{
+							"linux": "",
+							"macos": "darwin-",
+							"windows": "windows-",
+						}[.os | ascii_downcase]
+						// empty
+					) + (
+						{
+							"amd64": "amd64",
+							"arm32-v6": "arm32v6",
+							"arm32-v7": "arm32v7",
+							"arm64": "arm64v8",
+						}[.arch]
+						// empty
+					)
+				) as $arch
+				| { ($arch): {
+					url: ($urlBase + "/" + .file),
+					sha256: .sha256,
+					apkArch: ($arch | apk_arch),
+				} }
+			)
+			| { arches: add }
 		' || return "$?"
 	}
 	versions_hooks+=( hook_vultr-sha256 )
