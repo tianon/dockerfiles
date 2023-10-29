705e698385f8cb04155cf6097b7edeec3ecda43e
Tianon Gravi <admwiggin@gmail.com>
2023-10-28 20:35:35 -0700

    Improve c8dind test retry loop (one step closer to full retry.sh lolsob)

--- .test/tests/c8dind/run.sh
+++ .test/tests/c8dind/run.sh
@@ -35,18 +35,26 @@ ctr_() {
 # poor man's retry.sh
 tries=30
 while (( --tries > 0 )) && ! ctr_ version &> /dev/null; do
+	if ! running="$(docker inspect -f '{{ .State.Running }}' "$cid" 2>/dev/null)" || [ "$running" != 'true' ]; then
+		echo >&2 "$image stopped unexpectedly!"
+		( set -x && docker logs "$cid" ) >&2 || true
+		false
+	fi
+	echo >&2 -n .
 	sleep 1
 done
 ctr_ version > /dev/null
 
+testImage='docker.io/tianon/true:yoloci'
+
 [ "$(ctr_ image ls -q | wc -l)" = '0' ]
-ctr_ content fetch docker.io/tianon/true:yoloci
+ctr_ content fetch "$testImage"
 [ "$(ctr_ image ls -q | wc -l)" = '1' ]
 
 [ "$(ctr_ container ls -q | wc -l)" = '0' ]
 
-ctr_ run --rm docker.io/tianon/true:yoloci test
-ctr_ run --rm docker.io/tianon/true:yoloci test
-ctr_ run --rm docker.io/tianon/true:yoloci test
+ctr_ run --rm "$testImage" test
+ctr_ run --rm "$testImage" test
+ctr_ run --rm "$testImage" test
 
 [ "$(ctr_ container ls -q | wc -l)" = '0' ]
