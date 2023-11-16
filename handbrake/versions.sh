f6a03eac13b40d27764c7f3df19a2f7f72f094be
Tianon Gravi <admwiggin@gmail.com>
2023-11-15 21:25:46 -0800

    Add super basic handbrake hook to skip tags that aren't releases yet

--- handbrake/versions.sh
+++ handbrake/versions.sh
@@ -7,7 +7,13 @@ dir="$(readlink -ve "$BASH_SOURCE")"
 dir="$(dirname "$dir")"
 source "$dir/../.libs/git.sh"
 
-versions_hooks+=( hook_no-prereleases )
+# make sure the releases have published sources (difference between "tagged" and "released")
+hook_hb-missing-source() {
+	local version="$3"
+	wget -q --spider "https://github.com/HandBrake/HandBrake/releases/download/$version/HandBrake-$version-source.tar.bz2.sig"
+}
+
+versions_hooks+=( hook_no-prereleases hook_hb-missing-source )
 
 json="$(git-tags 'https://github.com/HandBrake/HandBrake.git')"
 
