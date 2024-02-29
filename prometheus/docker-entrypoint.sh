3b47e036603e3ebb84cd5ab9ebe4e1014f1b038f
Tianon Gravi <admwiggin@gmail.com>
2024-02-28 21:59:01 -0800

    Remove values that are already implied by the defaults (with workdir set appropriately)
    This is the easy fix for:
    
        Error parsing command line arguments: flag 'storage.tsdb.path' cannot be repeated

--- prometheus/docker-entrypoint.sh
+++ prometheus/docker-entrypoint.sh
@@ -7,8 +7,6 @@ if [ "$#" -eq 0 ] || [ "$1" = 'prometheus' ] || [[ "$1" = '-'* ]]; then
 	fi
 	args=(
 		prometheus
-		'--config.file=/opt/prometheus/prometheus.yml'
-		'--storage.tsdb.path=/opt/prometheus/data'
 		'--web.console.libraries=/opt/prometheus/console_libraries'
 		'--web.console.templates=/opt/prometheus/consoles'
 	)
