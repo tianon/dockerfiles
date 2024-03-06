0e47e284e85e8820ec5163d7c3197bd11dd84304
Tianon Gravi <admwiggin@gmail.com>
2024-03-05 21:59:21 -0800

    Scrape buildkit (minimal) Go version

--- buildkit/versions.sh
+++ buildkit/versions.sh
@@ -10,7 +10,13 @@ source "$dir/../.libs/git.sh"
 versions_hooks+=( hook_no-prereleases )
 
 json="$(
-	git-tags 'https://github.com/moby/buildkit.git'
+	json="$(git-tags 'https://github.com/moby/buildkit.git')"
+
+	commit="$(jq <<<"$json" -r '.commit')"
+	go="$(wget -qO- "https://github.com/moby/buildkit/raw/$commit/go.mod")"
+	go="$(awk <<<"$go" '$1 == "go" { print $2; exit }')"
+	echo >&2 "buildkit go: $go"
+	jq <<<"$json" -c --arg go "$go" '.go = { version: $go }'
 )"
 
 jq <<<"$json" '.' > versions.json
