68128b68d7fe8f0b1a3157b2e02ba87cfc2a6d9e
Tianon Gravi <admwiggin@gmail.com>
2024-09-06 14:54:23 -0700

    Update `docker-save-oci-layout.sh`

--- .test/tests/buildkitd/run.sh
+++ .test/tests/buildkitd/run.sh
@@ -36,8 +36,8 @@ docker buildx prune --builder "$builderName" --all --force
 # now that we've got the basic stuff out of the way, let's test the image's ability to transparently be a frontend instead
 tmp="$(mktemp -d)" # cleaned up in 'trap' above
 # https://github.com/tianon/docker-bin/blob/master/docker-save-oci-layout.sh
-saveMe='https://github.com/tianon/docker-bin/raw/fc5938cfdf70fc2b443e94d7a6768e50c4485dda/docker-save-oci-layout.sh'
-saveMeSha='08e0f9b0f69f44e40b045d65c38d94f3cd6b5b196c83190075c3593e0f37d871'
+saveMe='https://github.com/tianon/docker-bin/raw/67e6dbb725b3df2def1ac74820b61d9cefedc090/docker-save-oci-layout.sh'
+saveMeSha='baca98f706f58f6be5c327ce9b3ab7290b917b31dd7b354d397d28d2ce6fff97'
 wget -qO "$tmp/save.sh" "$saveMe"
 sha256sum <<<"$saveMeSha *$tmp/save.sh" --strict --check --quiet -
 chmod +x "$tmp/save.sh"
