83bc6e6e56890e73f284c431853bd3fcaf317000
Tianon Gravi <admwiggin@gmail.com>
2025-01-05 20:38:19 -0800

    Update to Alpine 3.21

--- ymlfmt/versions.sh
+++ ymlfmt/versions.sh
@@ -9,7 +9,7 @@ source "$dir/../.libs/pypi.sh"
 
 versions_hooks+=( hook_no-prereleases )
 
-export TIANON_PYTHON_FROM_TEMPLATE='python:%%PYTHON%%-alpine3.20'
+export TIANON_PYTHON_FROM_TEMPLATE='python:%%PYTHON%%-alpine3.21'
 
 json="$(pypi 'ruamel.yaml')"
 
