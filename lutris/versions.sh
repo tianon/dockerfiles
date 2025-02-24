60251523e18aa7e6c069cb2d2b7624d689f879af
Tianon Gravi <admwiggin@gmail.com>
2025-02-24 09:34:37 -0800

    Skip lutris releases missing .deb files

--- lutris/versions.sh
+++ lutris/versions.sh
@@ -8,6 +8,12 @@ dir="$(dirname "$dir")"
 source "$dir/../.libs/git.sh"
 
 versions_hooks+=( hook_no-prereleases )
+hook_missing-deb() {
+	local version="$3"
+	local debVer="${version//-/_}"; \
+	wget -q -O /dev/null --spider "https://github.com/lutris/lutris/releases/download/v${version}/lutris_${debVer}_all.deb"
+}
+versions_hooks+=( hook_missing-deb )
 
 json="$(git-tags 'https://github.com/lutris/lutris.git')"
 
