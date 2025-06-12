d4fdf9ba63b22c0903935e74e65d335c135cb373
Tianon Gravi <admwiggin@gmail.com>
2025-06-11 19:37:48 -0700

    Remove ppc64le from acarsdec (fails to build and I do not care)

--- adsb/gsl.sh
+++ adsb/gsl.sh
@@ -18,5 +18,5 @@ echo "Architectures: $fromArches"
 
 versionedEnvVariantEntry "$dir/acarsdec" acarsdec 'ACARSDEC_VERSION' acarsdec
 from="$(awk '$1 == "FROM" { print $2; exit }' "$dir/acarsdec/Dockerfile")" # TODO multi-stage build??
-fromArches="$(bashbrew remote arches --json "$from" | jq -rc '.arches | keys | join(", ")')"
+fromArches="$(bashbrew remote arches --json "$from" | jq -rc '.arches | keys | map(select(IN("ppc64le") | not)) | join(", ")')"
 echo "Architectures: $fromArches"
