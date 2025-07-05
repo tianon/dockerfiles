ddeb3255eb68d95bd476852777f915d70d393a51
Tianon Gravi <admwiggin@gmail.com>
2025-07-05 01:12:17 -0700

    Ignore tags called "master" in rtl-433 too

--- rtl-433/versions.sh
+++ rtl-433/versions.sh
@@ -10,7 +10,12 @@ source "$dir/../.libs/git.sh"
 versions_hooks+=( hook_no-prereleases )
 
 json="$(
-	hook_no-nightly() { [ "$3" != 'nightly' ]; }
+	hook_no-nightly() {
+		case "$3" in
+			'nightly') return 1 ;;
+			'master') return 1 ;; # this is diabolical (I'm not sure if it was a mistake or intentional, but they pushed a tag named "master")
+		esac
+	}
 	versions_hooks+=( hook_no-nightly )
 	git-tags 'https://github.com/merbanan/rtl_433.git'
 )"
